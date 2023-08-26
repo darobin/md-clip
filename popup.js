6d358615ebcb4c2c0a0067a3833c7368d0944761
Robin Berjon <robin.berjon@nytimes.com>
2023-08-26 22:27:36 +0200

    'very basics'

---
+++ popup.js
@@ -0,0 +1,32 @@
+
+const but = document.querySelector('button');
+const titleInput = document.querySelector('#title');
+const urlInput = document.querySelector('#url');
+const descInput = document.querySelector('#desc');
+
+but.onclick = async () => {
+  let md = `- **[${titleInput.value}](${urlInput.value})**`;
+  if (descInput.value) md += `: ${descInput.value}`;
+  md += `\n`;
+  await navigator.clipboard.writeText(md);
+};
+
+async function run () {
+  const [tab] = await browser.tabs.query({ currentWindow: true, active: true });
+  if (!tab) alert(`No active tab. Weird.`);
+
+  titleInput.value = tab.title;
+  urlInput.value = cleanUpURL(tab.url);
+  // XXX desc requires access to the content
+}
+
+function cleanUpURL (str) {
+  const url = new URL(str);
+  url.hash = '';
+  for (const k of url.searchParams.keys()) {
+    if (/^utm/.test(k)) url.searchParams.delete(k);
+  }
+  return url.toString();
+}
+
+run();
